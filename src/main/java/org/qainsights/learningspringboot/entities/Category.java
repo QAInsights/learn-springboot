60e633ef0f12ea39ee41f1d359e115d8ccfdaae8
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2026-03-08 15:07:55 -0400

    Update

--- src/main/java/org/qainsights/learningspringboot/entities/Category.java
+++ src/main/java/org/qainsights/learningspringboot/entities/Category.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.jspecify.annotations.NonNull;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,11 +20,14 @@ import lombok.Setter;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(nullable = false)
-    private Long id;
+    @Column(nullable = false, name = "id")
+    private Byte id;
 
     @Column(name = "name", nullable = false, length = 25)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
 
 
 }
